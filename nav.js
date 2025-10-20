2ae72b8da91f256b1d8ee2691171cb7d3a18b899
tristan geary <tgeary22@ubishops.ca>
2025-10-20 08:26:51 -0400

    initial commit for lab6

---
+++ nav.js
@@ -0,0 +1,30 @@
+function splitAtRoot(path){
+    const url = new URL(path, location.origin);
+    const pathFromRoot = url.pathname;
+    document.write("<br>----> path from root: " + pathFromRoot)
+    return pathFromRoot
+}
+
+function setNav(current_path){
+
+	current_path = splitAtRoot(current_path);
+		
+	fetch("nav.html")
+		.then(r => r.text())
+		.then(html => {document.getElementById("main-nav").innerHTML = html;
+		
+		let nav = document.getElementById("main-nav");
+		
+		for (let child of nav.children){
+			if (child instanceof HTMLAnchorElement) {
+				let child_path = splitAtRoot(child.href); 
+				
+				if(child_path == current_path){
+					child.classList.add("current_page");
+				}
+			}
+		}
+		});
+
+}
+
